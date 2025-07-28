0170d2d69d562f63187b24bceadb597761ae7d76
Arghya Banerjee <109426226+iemnoobie@users.noreply.github.com>
2025-07-28 18:11:46 +0530

    Delete Project/Project directory

--- Project/Project/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO product(name, desc, brand, price, category, releasedate, available, quantity) VALUES
-                    ('Tata Nexon','A compact SUV with excellent safety features and performane','Tata Motors','750000.00','Cars','2024-01-15',true,50),
-                    ('Suzuki Swift','A popular hatchback with great fuel economy','Maruti Suzuki','550000.00','Cars','2024-02-01',true,100),
-                    ('Hyundai Creta','A stylish SUV with excellent safety features and performane','Hyundai','950000.00','Cars','2024-03-01',true,75);
-
